7fdbaed060f9dbc7d08fcc86642a8de79115c5ab
Katriin <katriin.poolakese@khk.ee>
2020-09-03 18:21:05 +0300

    Koostatud Laul klass ja testitud antud klassi objekti loomist

---
+++ laul.py
@@ -0,0 +1,12 @@
+class Laul:
+    '''klass Laul
+
+    klassi omadused (Atributes)
+    pealkiri (str) - laulu pealkiri
+    laulja (str) - laulu laulja (artist)
+    '''
+    def __init__(self, pealkiri, laulja):
+        self.pealkiri = pealkiri
+        self.laulja = laulja
+
+
